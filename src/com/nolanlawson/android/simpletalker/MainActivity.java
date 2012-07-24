712b720d071a58b9fe7421100e7a0c33a794f713
Nolan Lawson <nolan.lawson@gmail.com>
2012-07-24 21:02:33 +0200

    kill activity after done speaking

--- src/com/nolanlawson/android/simpletalker/MainActivity.java
+++ src/com/nolanlawson/android/simpletalker/MainActivity.java
@@ -1,23 +1,26 @@
 package com.nolanlawson.android.simpletalker;
 
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.speech.tts.TextToSpeech.OnUtteranceCompletedListener;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends Activity implements OnInitListener, OnClickListener {
+public class MainActivity extends Activity implements OnClickListener, OnInitListener, OnUtteranceCompletedListener {
 
     public static final String EXTRA_TEXT = "text";
     
     
     private TextView textView;
-    private TextToSpeech textToSpeech;
     private Button playAgainButton;
+    private TextToSpeech textToSpeech;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,6 +29,13 @@ public class MainActivity extends Activity implements OnInitListener, OnClickLis
         textView = (TextView) findViewById(android.R.id.text1);
         playAgainButton = (Button) findViewById(android.R.id.button1);
         playAgainButton.setOnClickListener(this);
+        
+        String text = getIntent() != null && getIntent().hasExtra(EXTRA_TEXT) 
+        	? getIntent().getStringExtra(EXTRA_TEXT) : getString(R.string.default_text);
+        	
+        textView.setText(text);
+        
+        textToSpeech = new TextToSpeech(this, this);
     }
 
     @Override
@@ -36,21 +46,10 @@ public class MainActivity extends Activity implements OnInitListener, OnClickLis
 
     @Override
     public void onInit(int status) {
+        textToSpeech.setOnUtteranceCompletedListener(this);
 	speak();
     }
     
-    @Override
-    public void onResume() {
-	super.onResume();
-	
-        String text = getIntent() != null && getIntent().hasExtra(EXTRA_TEXT) 
-        	? getIntent().getStringExtra(EXTRA_TEXT) : getString(R.string.default_text);
-        	
-        textView.setText(text);
-        
-        textToSpeech = new TextToSpeech(this, this);
-    }
-
     @Override
     public void onClick(View v) {
 	// play it again clicked
@@ -59,8 +58,15 @@ public class MainActivity extends Activity implements OnInitListener, OnClickLis
     
     private void speak() {
 	String text = textView.getText().toString();
-	textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+	
+	HashMap<String, String> params = new HashMap<String, String>();
+	params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "meaninglessString");
+	textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, params);
+	
     }
 
-    
+    @Override
+    public void onUtteranceCompleted(String arg0) {
+	finish();
+    }
 }
