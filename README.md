7953919424aa006faefa4f7db2dadcbc9c327469
Nolan Lawson <nolan.lawson@gmail.com>
2012-07-27 18:50:17 +0200

    add ability to specify a song as well

--- README.md
+++ README.md
@@ -15,7 +15,10 @@ Overview
 Simple application, intended to be used from the command line to speak aloud some text.  Can be used via adb like so:
 
 ```
-adb shell am start -n com.nolanlawson.android.simpletalker/.MainActivity -e text "This is my text"
+adb shell am start -n com.nolanlawson.android.simpletalker/.MainActivity \
+    -e text "This is my text" \
+    -e song "/sdcard/someSong.mp3" \
+    -e songDuration 60
 ```
 
 [1]: http://sam.zoy.org/wtfpl/
--- src/com/nolanlawson/android/simpletalker/MainActivity.java
+++ src/com/nolanlawson/android/simpletalker/MainActivity.java
@@ -1,8 +1,13 @@
 package com.nolanlawson.android.simpletalker;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
@@ -16,11 +21,18 @@ import android.widget.TextView;
 public class MainActivity extends Activity implements OnClickListener, OnInitListener, OnUtteranceCompletedListener {
 
     public static final String EXTRA_TEXT = "text";
+    public static final String EXTRA_SONG = "song";
+    public static final String EXTRA_SONG_DURATION = "songDuration";
     
+    private static final int DEFAULT_DURATION = 10; // 10 seconds
     
+    private String text;
+    private String song;
+    private long songDuration;
     private TextView textView;
     private Button playAgainButton;
     private TextToSpeech textToSpeech;
+    private MediaPlayer mPlayer;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,10 +42,17 @@ public class MainActivity extends Activity implements OnClickListener, OnInitLis
         playAgainButton = (Button) findViewById(android.R.id.button1);
         playAgainButton.setOnClickListener(this);
         
-        String text = getIntent() != null && getIntent().hasExtra(EXTRA_TEXT) 
+        text = getIntent() != null && getIntent().hasExtra(EXTRA_TEXT) 
         	? getIntent().getStringExtra(EXTRA_TEXT) : getString(R.string.default_text);
         	
         textView.setText(text);
+        
+        song = getIntent() != null && getIntent().hasExtra(EXTRA_SONG) 
+        	? getIntent().getStringExtra(EXTRA_SONG) : null;
+        	
+        songDuration = getIntent() != null && getIntent().hasExtra(EXTRA_SONG_DURATION)
+        	? Long.parseLong(getIntent().getStringExtra(EXTRA_SONG_DURATION)) : DEFAULT_DURATION;
+        
         
         textToSpeech = new TextToSpeech(this, this);
     }
@@ -47,26 +66,68 @@ public class MainActivity extends Activity implements OnClickListener, OnInitLis
     @Override
     public void onInit(int status) {
         textToSpeech.setOnUtteranceCompletedListener(this);
-	speak();
+	speakAndPlayMusic();
     }
     
     @Override
     public void onClick(View v) {
 	// play it again clicked
-	speak();
+	speakAndPlayMusic();
+    }
+    
+    private void speakAndPlayMusic() {
+	new AsyncTask<Void, Void, Void>(){
+
+	    @Override
+	    protected Void doInBackground(Void... params) {
+		speakAndPlayMusicInBackground();
+		return null;
+	    }
+	}.execute((Void)null);
+    }
+    
+    private void speakAndPlayMusicInBackground() {
+	
+	File file;
+	if (song != null && (file = new File(song)).exists()) {
+	    // play song first
+    	    mPlayer = MediaPlayer.create(this, Uri.fromFile(file));
+    	    mPlayer.start();
+    	    try {
+		Thread.sleep(TimeUnit.SECONDS.toMillis(songDuration));
+	    } catch (InterruptedException e) { /* shouldn't happen */ }
+    	    mPlayer.stop();
+    	    try {
+		Thread.sleep(1000); // pause between music stop and TTS
+	    } catch (InterruptedException e) { /* shouldn't happen */ }
+    	    speak();
+	} else {
+	    // just speak
+	    speak();
+	}
     }
     
     private void speak() {
-	String text = textView.getText().toString();
-	
 	HashMap<String, String> params = new HashMap<String, String>();
-	params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "meaninglessString");
-	textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, params);
-	
+    	params.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "meaninglessString");
+    	textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, params);
     }
 
     @Override
     public void onUtteranceCompleted(String arg0) {
 	finish();
+    }
+    
+    
+    
+    @Override
+    public void onStop() {
+	super.onStop();
+	if (mPlayer != null) {
+	    mPlayer.release();
+	}
+	if (textToSpeech != null) {
+	    textToSpeech.shutdown();
+	}
     }
 }
